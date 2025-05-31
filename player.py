b4e17a0cc0630a2fae39a553ef5e3655375b8f1c
Wisam Odish <wirrexx@gmail.com>
2025-05-31 11:29:50 +0200

    added new methods for rotation and move in player.py

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
 import pygame
+from constants import (PLAYER_TURN_SPEED, 
+                       PLAYER_RADIUS, 
+                       PLAYER_SPEED)
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -19,3 +21,24 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt  
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
