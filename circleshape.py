b10b51517e797d68f1a7da61ddceaf520b428394
Wisam Odish <wirrexx@gmail.com>
2025-05-31 13:00:03 +0200

    Cleaned code

--- circleshape.py
+++ circleshape.py
@@ -26,4 +26,4 @@ class CircleShape(pygame.sprite.Sprite):
     def collision(self, other):
         if self.position.distance_to(other.position) < (self.radius + other.radius):
             return True
-        return False
+        return False 
