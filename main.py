b8123f364f67258c8e12028701ce8ab7fc4356e6
Wisam Odish <wirrexx@gmail.com>
2025-05-31 09:16:51 +0200

    Main.py created + Constants.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import constants
+
+def main():
+    print("Starting Asteroids!")
+    height = constants.SCREEN_HEIGHT
+    width = constants.SCREEN_WIDTH
+
+    print(f"Screen width: {width}\nScreen height: {height}")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
